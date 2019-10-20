9fd8ebf0857d1b9eeb7e4348344fd8c83be97649
minhduc <minh.duc.3701@gmail.com>
2019-10-20 13:44:08 +0700

    Create Component

---
+++ src/reducers/products.js
@@ -0,0 +1,15 @@
+import * as Types from "./../constants/ActionTypes";
+
+let initialState = [];
+
+const products = (state = initialState, action) => {
+  switch (action.type) {
+    case value:
+      return [...state];
+
+    default:
+      return [...state];
+  }
+};
+
+export default products;
